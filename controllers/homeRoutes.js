66a9c7068ff6d2cc685d71b485cdab9e37ca8e25
Spencer Cox <spencox@gmail.com>
2023-12-29 10:46:40 -0600

    added files to get basic routing and splash pages set up. Currently the site will open to a generic splash page

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+//const { Users } = require('../models');
+const withAuth = require('../utils/auth');
+
+// view home splash page
+router.get('/', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('splash', {
+    title: 'Welcome to BSTC',
+    style: 'splash.css'
+  });
+});
+
+// // view user dashboard page with user data
+// router.get('/dashboard', withAuth, async (req, res) => {
+//   console.log('ID OF USER: ' + req.session.user_id)
+//   try {
+//     const user = await Users.findByPk(req.session.user_id);
+//     console.log("USER: " + user);
+
+//     const userData = user.get({ plain: true });
+
+//     console.log("USER DATA: " + userData);
+
+//     res.render('dashboard', {
+//       userData,
+//       title: `${userData.username}'s Dashboard`,
+//       style: 'dashboard.css',
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// // github repo search page
+// router.get('/bugs', withAuth, (req, res) => {
+//   res.render('bugs', {
+//     title: 'Search for Bugs',
+//     style: 'dashboard.css',
+//     logged_in: req.session.logged_in
+//   });
+// });
+
+// // view login page
+// router.get('/login', (req, res) => {
+//   if (req.session.logged_in) {
+//     res.redirect('/');
+//     return;
+//   }
+//   res.render('login', {
+//     title: 'User Login',
+//     style: 'login.css'
+//   });
+// });
+
+// // view sign up page
+// router.get('/signup', (req, res) => {
+//   if (req.session.logged_in) {
+//     res.redirect('/');
+//     return;
+//   }
+//   res.render('signup', {
+//     title: 'Sign Up for BBB',
+//     style: 'signup.css'
+//   });
+// });
+
+module.exports = router;
